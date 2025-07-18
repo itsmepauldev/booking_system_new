8722c168df41c8cbf69028f7ed8a12abb97214e0
Itspauldev <autorice65@gmail.com>
2025-07-18 20:18:21 +0800

    Your commit message here

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
