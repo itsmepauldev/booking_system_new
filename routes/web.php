4be75dbe111b9aaccad196948527c4bcd5f1d375
Itspauldev <autorice65@gmail.com>
2025-07-18 20:19:59 +0800

    New

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('register');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
